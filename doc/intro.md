d9ea33917444b17eb3fa3d6d42d99c042c9627ae
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-06-18 20:13:36 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to just_married
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/just_married/core_test.clj
@@ -0,0 +1,7 @@
+(ns just-married.core-test
+  (:require [clojure.test :refer :all]
+            [just-married.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
