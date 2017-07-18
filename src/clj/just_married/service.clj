c1a17f37efc69e359f3129c79f1a41bbbf732696
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-07-18 13:40:16 +0100

    add service/server.clj and some extra project.clj settings

---
+++ src/clj/just_married/service.clj
@@ -0,0 +1,44 @@
+(ns just-married.service
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.body-params :as body-params]
+            [ring.util.response :as ring-resp]))
+
+(defn home-page
+  [request]
+  (http/json-response "Getting married"))
+
+(def routes #{["/" :get (conj common-interceptors `home-page)]})
+
+(def service {:env :prod
+              ;; You can bring your own non-default interceptors. Make
+              ;; sure you include routing and set it up right for
+              ;; dev-mode. If you do, many other keys for configuring
+              ;; default interceptors will be ignored.
+              ;; ::http/interceptors []
+              ::http/routes routes
+
+              ;; Uncomment next line to enable CORS support, add
+              ;; string(s) specifying scheme, host and port for
+              ;; allowed source(s):
+              ;;
+              ;; "http://localhost:8080"
+              ;;
+              ;;::http/allowed-origins ["scheme://host:port"]
+
+              ;; Root for resource interceptor that is available by default.
+              ::http/resource-path "/public"
+
+              ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
+              ::http/type :jetty
+              ;;::http/host "localhost"
+              ::http/port 8080
+              ;; Options to pass to the container (Jetty)
+              ::http/container-options {:h2c? true
+                                        :h2? false
+                                        ;:keystore "test/hp/keystore.jks"
+                                        ;:key-password "password"
+                                        ;:ssl-port 8443
+                                        :ssl? false}})
+
+ 
