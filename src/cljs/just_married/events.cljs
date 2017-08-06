caf42996d7575dce09c1dfef5641af1f5385cd2a
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-08-06 19:06:52 +0100

    confirm

--- src/cljs/just_married/events.cljs
+++ src/cljs/just_married/events.cljs
@@ -1,6 +1,7 @@
 (ns just-married.events
   (:require [re-frame.core :as re-frame :refer [dispatch reg-event-db]]
-            [just-married.db :as db]))
+            [just-married.db :as db]
+            [ajax.core :refer [GET]]))
 
 (reg-event-db
  :initialize-db
@@ -21,3 +22,17 @@
  :email
  (fn [db [_ name]]
    (assoc db :email name)))
+
+(defn confirm
+  [coming db]
+  (print "Calling function to record coming"))
+
+(reg-event-db
+ :coming
+ (fn [db [_]]
+   (confirm db true)))
+
+(reg-event-db
+ :not-coming
+ (fn [db [_]]
+   (confirm db false)))
