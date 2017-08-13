cc04bd94b5384d241568d1d93066ea9f49648188
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-08-13 16:51:56 +0100

    use bootstrap from cdn

--- heroku.sh
+++ heroku.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 set -ex
+# use a different port to avoid overlapping
 
 lein uberjar
-java $JVM_OPTS -cp target/uberjar/just-married.jar \
-     clojure.main -m just-married.server
+PORT=5001 java $JVM_OPTS \
+    -cp target/uberjar/just-married.jar clojure.main \
+     -m just-married.server
