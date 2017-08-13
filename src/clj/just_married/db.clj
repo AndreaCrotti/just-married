c8e2dff3c9f8d738cbfb24e844450d731f6f5b58
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-08-13 19:47:59 +0100

    script to import data from CSV

--- src/clj/just_married/db.clj
+++ src/clj/just_married/db.clj
@@ -1,7 +1,8 @@
 (ns just-married.db
   (:require [clojure.java.jdbc :as j]
             [honeysql.core :as honey]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [honeysql.helpers :as helpers]))
 
 (def DEFAULT-DB-URL "postgresql://just_married:just_married@localhost:5440/just_married")
 
@@ -11,6 +12,13 @@
 (def all-people
   {:select [:first-name] :from [:people]})
 
+
 ;; sample queries
 ;; (j/execute! db-conn ["TRUNCATE TABLE people CASCADE"])
 ;; (j/query db-conn (honey/format all-people))
+
+;; (-> (helpers/insert-into :family)
+;;     (helpers/columns :family-name :second)
+;;     (helpers/values
+;;      [["hello" "world"]])
+;;     honey/format)
