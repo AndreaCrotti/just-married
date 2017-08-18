9508dcad7b9406783e63b76de74c214ae69c5236
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-08-18 15:24:12 +0100

    sample test

--- resources/migrations/20170816223405-confirmations.up.sql
+++ resources/migrations/20170816223405-confirmations.up.sql
@@ -9,6 +9,5 @@ CREATE TABLE confirmation (
        confirmed_by INTEGER NOT NULL,
        confirmed_in_date TIMESTAMP DEFAULT CURRENT_DATE,
 
-       FOREIGN KEY(family_id) REFERENCES family(id),
        FOREIGN KEY(confirmed_by) REFERENCES people(id)
 );
