f0906ceb31c3703d85794a328677047fc1971d4e
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-10-08 15:08:41 +0100

    missing dir-locals file

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (cider-cljs-lein-repl . "(do (require 'figwheel-sidecar.repl-api) (figwheel-sidecar.repl-api/start-figwheel!) (figwheel-sidecar.repl-api/cljs-repl))")))
