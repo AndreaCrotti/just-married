cf74f7a54e91076889daab8f5cd79a2ad7796e36
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-01-06 17:55:11 +0000

    initial on click information

--- resources/public/map.js
+++ resources/public/map.js
@@ -5,19 +5,22 @@ var places = [
         'lat': 42.346799,
         'lng': 14.164534,
         'title': 'Palazzo Lepri',
-        'icon': 'images/rings_small.png'
+        'icon': 'images/rings_small.png',
+        'info': 'Palazzo Lepri'
     },
     {
         'lat': 42.423608,
         'lng': 14.231041,
         'title': 'Parco dei principi',
-        'icon': 'images/party_small.png'
+        'icon': 'images/party_small.png',
+        'info': 'Parco dei Principi'
     },
     {
         'lat': 42.421552,
         'lng': 14.230742,
         'title': 'Parc Hotel Villa Immacolata',
-        'icon': 'images/hotel_small.png'
+        'icon': 'images/hotel_small.png',
+        'info': 'Hotel Villa Immacolata'
     }
 ]
 var mapOptions = {
@@ -28,9 +31,15 @@ var mapOptions = {
 var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
 places.forEach(function(place) {
-    new google.maps.Marker({
+    var marker = new google.maps.Marker({
         position: new google.maps.LatLng(place.lat, place.lng),
         icon: place.icon,
         map: map
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content: place.info
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker)
     });
 });
