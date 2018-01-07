98d0934b01fe5cf38d88e46e59310e3110eaf1b1
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-01-07 19:34:30 +0000

    cleanup

--- src/cljs/just_married/home/language.cljs
+++ src/cljs/just_married/home/language.cljs
@@ -16,43 +16,6 @@
       (get "language" "en")
       keyword))
 
-(def dicts
-  "List of all the words/sentences that need localization"
-  {:en {:home "Home"
-        :timeline "Timeline"
-        :story "Our Story"
-        :find-us "Find Us"
-        :find-us-text "The wedding will be done in the beautiful Abruzzo region, between Chieti and Pescara."
-        :countdown "Countdown"
-        :accomodation "Accommodation"
-        :contacts "Contacts"
-        :add-to-calendar "Add to Calendar"
-        :date "27th May, 2018"
-        :days "Days"
-        :hours "Hours"
-        :minutes "Minutes"
-        :read-more "Read More"
-        :read-less "Read Less"
-
-        ;; timeline translation
-        :waiting "Waiting for the bride Aperitif"
-        }
-
-   :it {:home "Home"
-        :timeline "Programma"
-        :story "La Nostra Storia"
-        :find-us "Trovaci"
-        :countdown "Conto Alla Rovescia"
-        :accomodation "Dove Dormire"
-        :contacts "Contatti"
-        :add-to-calendar "Aggiungi al Calendario"
-        :date "27 Maggio 2018"
-        :days "Giorni"
-        :hours "Ore"
-        :minutes "Minuti"
-        :read-more "Leggi di piu'"
-        :read-less "Leggi di meno"}})
-
 (defn translate
   [dict key]
   ((tongue/build-translate dict) (get-language) key))
