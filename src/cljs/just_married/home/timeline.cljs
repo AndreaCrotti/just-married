f40ff072f092191da091737cb6ba273d5c63eddb
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-01-10 23:04:22 +0000

    icon explaining where each thing is

--- src/cljs/just_married/home/timeline.cljs
+++ src/cljs/just_married/home/timeline.cljs
@@ -1,25 +1,29 @@
 (ns just-married.home.timeline
   (:require [just-married.home.language :refer [translate]]))
 
+(def ^:private icons
+  {:lepri "images/rings_small.png"
+   :parco "images/party_small.png"})
+
 (def ^:private timeline-def
-  [["11:00" :waiting]
-   ["11:15" :civil-start]
-   ["12:15" :civil-end]
-   ["12:15 - 13:00" :cheers]
-   ["13:00" :grand-buffet]
-   ["14:00" :lunch-reception]
-   ["16:00" :cake-champagne]
-   ["16:30" :games-kick-off]
-   ["19:00" :evening-reception]
-   ["21:00" :music-party]
-   ["23:00" :final-cake]])
+  [["11:00" :lepri :waiting]
+   ["11:15" :lepri :civil-start]
+   ["12:15" :lepri :civil-end]
+   ["12:15" :lepri :cheers]
+   ["13:00" :parco :grand-buffet]
+   ["14:00" :parco :lunch-reception]
+   ["16:00" :parco :cake-champagne]
+   ["16:30" :parco :games-kick-off]
+   ["19:00" :parco :evening-reception]
+   ["21:00" :parco :music-party]
+   ["23:00" :parco :final-cake]])
 
 (def ^:private timeline-dict
   {:en {:waiting "Waiting for the bride aperitif"
         :timeline "Timeline"
         :civil-start "Civil cerimony starts"
         :civil-end "Civil cerimony ends"
-        :cheers "Cheers and Greetings to Mr and Mrs Andrea Crotti - Moving to Parco dei Principi"
+        :cheers "Cheers and Greetings to Mr and Mrs Andrea Crotti"
         :grand-buffet "Grand Buffet"
         :lunch-reception "Lunch Reception"
         :games-kick-off "Games kick-off"
@@ -48,7 +52,8 @@
   [:div.timeline {:id "timeline"}
    [:h3 (tr :timeline)]
    (into [:ul]
-         (for [[time key] timeline-def]
-           [:li
+         (for [[time place key] timeline-def]
+           [:li {:class (place icons)}
+            [:img.timeline__icon {:src (place icons)}]
             [:span.timeline__time time]
             [:span.timeline__msg (tr key)]]))])
