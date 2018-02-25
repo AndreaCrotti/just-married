73fe711e499261a53e1d9d644adbd3a183ea8e3e
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-02-25 22:27:19 +0000

    clarity

--- src/cljs/just_married/home/accommodation.cljs
+++ src/cljs/just_married/home/accommodation.cljs
@@ -15,14 +15,14 @@
 (def ^:private accommodation-dict
   {:en {:title    "Accommodation"
         :suggest  "We suggest booking at the Hotel Villa Immacolata."
-        :code     "The code to get a 10% discount is: \"matrimonio Verrucci\" and prices range from 58 € for the single to the 89 € for the triple room, breakfast included."
+        :code     "The code to get a 10% discount is: \"matrimonio Verrucci\" and prices range from 58 € (per night) for the single to the 89 € for the triple room, breakfast included."
         :distance "As you can see from the map from Villa Immacolata you can even walk to the party venue, and we will organise a Bus from there to the place of the ceremony."
         :contact  "Contact the hotel at %s for booking and more information."}
 
    :it {:title   "Dove Dormire"
         :suggest "Suggeriamo di pernottare all'Hotel Villa Immacolata."
         ;;TODO: verify if the code can be used directly on booking.com or how else
-        :code    "Il codice per ottenere uno sconto del 10% è: \"matrimonio Verrucci\", e i prezzi variano dai 58 € per la singola ai 89 € per la tripla, colazione inclusa."
+        :code    "Il codice per ottenere uno sconto del 10% è: \"matrimonio Verrucci\", e i prezzi variano dai 58 € (a notte) per la singola ai 89 € per la tripla, colazione inclusa."
 
         :distance "Come si puó vedere dalla mappa Villa Immacolata é a soli 5 minuti a piedi dal Parco dei Principi (dove si svolge la festa), e ci sarà un Pullman organizzato che porta dall'Hotel al luogo della cerimonia."
         :contact  "Potete contattare l'hotel alla mail %s per prenotare e per ulteriori informazioni"}})
