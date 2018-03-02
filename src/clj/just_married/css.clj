9dfc49bec4cca45abb2b641fe00db1643bb69ab1
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-03-02 12:52:30 +0000

    try to fix mobile and desktop padding

--- src/clj/just_married/css.clj
+++ src/clj/just_married/css.clj
@@ -1,29 +1,28 @@
 (ns just-married.css
   (:require [garden.def :refer [defstyles defcssfn]]
+            [garden.core :refer [css]]
             [garden.stylesheet :refer [at-media]]
             [just-married.shared :refer [sections]]))
 
-;; (def ^:private map-width "500px")
 (def ^:private map-height "500px")
 (def ^:private max-width-mobile "480px")
 
 ;; dynamically get the grid size from the actual sections declared
 (def num-sections (-> sections count))
 (def menu-size (format "repeat(%d, 1fr)" (inc num-sections)))
-
 (defn repeat-word
   [word times]
   (clojure.string/join " "
                        (take times (cycle [word]))))
 
 (def color-pallette
-  {:white "#FFFFFF"
-   :navy "#0e0e4c"
-   :light-grey "#DDDDDD"
-   :dark-red "#8B0000"
-   :gold "#FFd700"
-   :marsala "#955251"
-   :quote-background "#f9f9f9"
+  {:white             "#FFFFFF"
+   :navy              "#0e0e4c"
+   :light-grey        "#DDDDDD"
+   :dark-red          "#8B0000"
+   :gold              "#FFd700"
+   :marsala           "#955251"
+   :quote-background  "#f9f9f9"
    :quote-border-left "#ccc"})
 
 (def font-families
@@ -45,26 +44,29 @@
   {:background-color      (:dark-red color-pallette)
    :font-weight           "bolder"
    :display               "grid"
-   :grid-gap              "1em"
    :text-align            "center"
    :text-decoration       "none"
-   :margin-bottom         "50px"
-   :position              "sticky"
-   :top                   "0"
    ;; to avoid making it go behind the map
-   :z-index               100})
+   })
 
 (def ^:private navbar-grid-config
   ;; simply invert rows and columns while using the same grid
   ;; otherwise
   {:desktop (merge navbar-grid-config
                    {:grid-template-columns menu-size
-                    :grid-template-rows    "auto"})
+                    :position              "sticky"
+                    :top                   0
+                    :grid-template-rows    "auto"
+                    :grid-gap              "1em"
+                    :z-index               100})
 
    :mobile (merge navbar-grid-config
                   {:grid-template-columns "auto"
                    :grid-template-rows    menu-size
-                   :font-size             "1.9em"})})
+                   :font-size             "1.9em"
+                   :grid-gap              "5px"
+                   :position              "relative"
+                   :z-index               0})})
 
 (def common
   [[:a {:text-decoration "none"}]
@@ -72,7 +74,7 @@
 
 (def main-page
   [[:.container body-grid-config]
-   ;; [:.section {:grid-column "1"}]
+   [:.section {:padding-top "40px"}]
    [:.find-us {:grid-column "2"}]
    [:.countdown {:grid-column "2"}]
    [:.timeline {:grid-column "2"}]
@@ -87,45 +89,45 @@
    [:.countdown__internal {:text-align "center"
                            :width      "500px"}]
 
-   [:.timer {:font-size "1.5em"
-             :font-family (:alex-brush font-families)
+   [:.timer {:font-size    "1.5em"
+             :font-family  (:alex-brush font-families)
              :border-color (:dark-red color-pallette)
-             :margin-top "10px"}]
+             :margin-top   "10px"}]
 
    [:.button__add-to-calendar {:background-color (:dark-red color-pallette)
-                               :color (:white color-pallette)}]
+                               :color            (:white color-pallette)}]
 
    [:.timeline__time {:padding-right "10px"
-                      :font-weight "bolder"}]
+                      :font-weight   "bolder"}]
 
-   [:.timeline__icon {:width "22px"
+   [:.timeline__icon {:width         "22px"
                       :padding-right "5px"}]
 
    [:.bank_table {:font-family (:open-sans font-families)}]
 
    [:th {:padding "4px"}]
-   [:td {:padding "5px"
+   [:td {:padding    "5px"
          :text-align "left"
-         :border (format "0.8px solid %s" (:navy color-pallette))}]
+         :border     (format "0.8px solid %s" (:navy color-pallette))}]
 
    [:.accommodation__villa-immacolata {:padding-bottom "20px"}]
 
    [:ul {:list-style "none"}]
 
-   [:.rvsp__form {:display "grid"
-                  :grid-gap "10px"
+   [:.rvsp__form {:display               "grid"
+                  :grid-gap              "10px"
                   :grid-template-columns "auto auto"
-                  :grid-template-rows "auto auto auto auto auto auto"}]
+                  :grid-template-rows    "auto auto auto auto auto auto"}]
 
-   [:.rvsp__confirm {:width "200px"
-                     :border-radius "4px"
-                     :padding "10px"
-                     :font-size "1.3em"
+   [:.rvsp__confirm {:width           "200px"
+                     :border-radius   "4px"
+                     :padding         "10px"
+                     :font-size       "1.3em"
                      :text-decoration "none"
-                     :display "inline-block"
-                     :border "none"
-                     :cursor "hand"
-                     :color (:white color-pallette)}]
+                     :display         "inline-block"
+                     :border          "none"
+                     :cursor          "hand"
+                     :color           (:white color-pallette)}]
 
    [:.rvsp__confirm:hover {:font-size "1.5em"}]
 
@@ -133,35 +135,35 @@
 
    [:.rvsp__not_coming {:background-color (:navy color-pallette)}]
 
-   [:blockquote:before {:color (:quote-border-left color-pallette)
-                        :content "open-quote"
-                        :font-size "3em"
-                        :line-height ".1em"
-                        :margin-right ".25em"
+   [:blockquote:before {:color          (:quote-border-left color-pallette)
+                        :content        "open-quote"
+                        :font-size      "3em"
+                        :line-height    ".1em"
+                        :margin-right   ".25em"
                         :vertical-align "-0.4em"}]
 
-   [:blockquote {:background (:quote-background color-pallette)
-                 :padding "0.5em 10px"
-                 :font-size "1em"
+   [:blockquote {:background  (:quote-background color-pallette)
+                 :padding     "0.5em 10px"
+                 :font-size   "1em"
                  :font-family (:open-sans font-families)
                  :border-left (format "10px solid %s" (:quote-border-left color-pallette))
-                 :quotes "\201C\201D\2018\2019"}]
+                 :quotes      "\201C\201D\2018\2019"}]
 
-   [:blockquote:after {:color (:quote-border-left color-pallette)
-                       :content "close-quote"
-                       :font-size "2em"
-                       :line-height ".1em"
-                       :margin-right ".25em"
+   [:blockquote:after {:color          (:quote-border-left color-pallette)
+                       :content        "close-quote"
+                       :font-size      "2em"
+                       :line-height    ".1em"
+                       :margin-right   ".25em"
                        :vertical-align "-.2em"
-                       :box-shadow (format "0 0 20px %s" (:dark-red color-pallette))}]
+                       :box-shadow     (format "0 0 20px %s" (:dark-red color-pallette))}]
 
    [:app {:background-color (:marsala color-pallette)}]
 
    ;; find us settings
-   [:.google-map {:width "90%"
-                  :height map-height
-                  :padding "15px"
-                  :box-shadow (format "10px 10px 20px %s" (:light-grey color-pallette))
+   [:.google-map {:width         "90%"
+                  :height        map-height
+                  :padding       "15px"
+                  :box-shadow    (format "10px 10px 20px %s" (:light-grey color-pallette))
                   :border-radius "10px"}]
 
    [:.find-us-text {:padding-bottom "30px"}]
@@ -171,8 +173,8 @@
     ;; FIXME: should be less generic here
     [:a {:color (:white color-pallette)}]
 
-    [:a:hover {:color (:gold color-pallette)
-               :border-left "1px solid white"
+    [:a:hover {:color        (:gold color-pallette)
+               :border-left  "1px solid white"
                :border-right "1px solid white"}]]])
 
 (def enter-page
@@ -211,9 +213,10 @@
                                    :padding-right "150px"}]])
 
 (def responsive
-  [[(at-media {:screen true
+  [[(at-media {:screen           true
                :max-device-width max-width-mobile}
 
+              [:.section {:padding-top "5px"}]
               [:.navbar__container (:mobile navbar-grid-config)]
               [:.container body-grid-config])]])
 
