df259a80e9a6fddbe1b380d389c6024732acc698
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-03-12 21:15:40 +0000

    figwheel

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,10 +2,12 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
+  (cider-default-cljs-repl . "Figwheel")
   (cider-cljs-lein-repl . "(do (require 'figwheel-sidecar.repl-api) (figwheel-sidecar.repl-api/start-figwheel!) (figwheel-sidecar.repl-api/cljs-repl))"))
  (clojure-mode
   (clojure-align-forms-automatically . t))
  (clojurescript-mode
   (clojure-align-forms-automatically . t)))
+
 
 
