75165f80079aea19ba8b457fc86e774d6dc26cbd
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-03-12 13:17:12 +0000

    initial sample config

---
+++ src/clj/just_married/system.clj
@@ -0,0 +1,30 @@
+(ns just-married.system
+  (:require [integrant.core :as ig]
+            [environ.core :refer [env]]
+            [ring.adapter.jetty :as jetty]))
+
+(def ^:private default-port 3000)
+
+(defn- get-port
+  []
+  (Integer. (or (env :port) default-port)))
+
+(def config
+  {:adapter/jetty {:port (get-port)}})
+
+(defmethod ig/init-key :adapter/jetty
+  ;; where does the handler come from in this case?
+  [_ {:keys [handler] :as opts}]
+  (jetty/run-jetty handler {:port (get-port)}))
+
+(defmethod ig/halt-key! :adapter/jetty
+  [_ server]
+  (.stop server))
+
+(defmethod ig/init-key :garden/compiler
+  [_ {:keys [handler] :as opts}]
+  )
+
+;; this is how the system is booted up
+;; (def system
+;;   (ig/init config))
