6f23d1bb9322a045b272ecc9baebc0836b336e72
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-06-16 14:30:03 +0100

    some more code snippets

--- src/clj/just_married/api/labels.clj
+++ src/clj/just_married/api/labels.clj
@@ -13,21 +13,16 @@
 (def ^:private file-name "labels.pdf")
 (def ^:private default-font "Helvetica")
 
-(defn labels-sql
-  []
+(def labels-sql
   (-> (h/select :group_name :country :address)
       (h/from :guests-group)
       (h/where [:and
-                [:= :invitation_sent false]
-                ;; could add this if we don't need labels for manually handed over anyway
-                ;; [:not= :address nil]
-                ;; [:not= :country nil]
-                ])
+                [:= :invitation_sent false]])
       (sql/format)))
 
 (defn get-labels!
   []
-  (jdbc/query (db/db-spec) (labels-sql)))
+  (jdbc/query (db/db-spec) labels-sql))
 
 (def pdf-options
   {:title                  "Address List"
@@ -38,9 +33,8 @@
    :size                   :a4
    :font                   {:size     12
                             :family   default-font
-                            ;;TODO: change the font in this way if you want to
-                            ;; :ttf-name "resources/public/fonts/OpenSans-Regular.ttf"
                             :encoding :unicode}
+
    :register-system-fonts? true})
 
 (def table-options
